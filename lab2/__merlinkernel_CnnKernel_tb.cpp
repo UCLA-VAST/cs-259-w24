dcf524d970cb265fa54f5986b223bb79de0a0391
S12P <26119556+S12P@users.noreply.github.com>
2024-01-26 19:10:25 -0800

    lab2

--- lab2/__merlinkernel_CnnKernel_tb.cpp
+++ lab2/__merlinkernel_CnnKernel_tb.cpp
@@ -1,240 +1,136 @@
-#define __constant
-#define __kernel
-#define __global
-#include "memcpy_128_1d.h"
-#include "memcpy_512_1d.h"
-#define SIZE_1 228
-#include "memcpy_128_2d.h"
-#undef SIZE_1
-#define SIZE_1 112
-#define SIZE_2 112
-#include "memcpy_512_3d.h"
-#undef SIZE_1
-#undef SIZE_2
-#include <string.h> 
+#include <ap_int.h>
 
-#include "merlin_type_define.h"
+// parameters
+#define kNum            (256)
+#define kKernel         (5)
+#define kImSize         (224)
+#define kInImSize       (228)
+#define kOutImSize      (112)
+#define max(X,Y) ((X)>(Y)?(X):(Y))
+// software golden model
+void CnnKernelSoftware(
+    const float input[kNum][kInImSize][kInImSize],
+    const float weight[kNum][kNum][kKernel][kKernel],
+    const float bias[kNum],
+    float output[kNum][kOutImSize][kOutImSize]
+  ) {
 
+  // Allocate memory on heap to avoid stack overflow.
+  static float C[kNum][kImSize][kImSize];
 
-
-
-
-
-
-// Original: #pragma ACCEL kernel
-static int CnnKernel_dummy_pos;
-
-static void merlin_memcpy_0(float dst[256][256][5][5],int dst_idx_0,int dst_idx_1,int dst_idx_2,int dst_idx_3,float src[1638400],int src_idx_0,unsigned int len,unsigned int max_len)
-{
-  
-#pragma HLS inline off
-  
-#pragma HLS function_instantiate variable=dst_idx_0,dst_idx_1,dst_idx_2,dst_idx_3,src_idx_0
-  unsigned long i;
-  unsigned long total_offset1 = (((0 * 256 + dst_idx_0) * 256 + dst_idx_1) * 5 + dst_idx_2) * 5 + dst_idx_3;
-  unsigned long total_offset2 = 0 * 1638400 + src_idx_0;
-  merlinL0:
-  for (i = 0; i < len / 4; ++i) {
-    
-#pragma HLS PIPELINE II=1
-    
-#pragma HLS LOOP_TRIPCOUNT max=1638400
-    dst[(total_offset1 + i) / 5 / 5 / 256][(total_offset1 + i) / 5 / 5 % 256][(total_offset1 + i) / 5 % 5][(total_offset1 + i) % 5] = src[total_offset2 + i];
-  }
-}
-extern "C" { 
-
-void CnnKernel(class ap_uint< 128 > merlin_input[3326976],float weight[1638400],const class ap_uint< 512 > bias[16],class ap_uint< 512 > merlin_output[200704])
-{
-  
-#pragma HLS INTERFACE m_axi port=bias offset=slave depth=16 bundle=merlin_gmem_CnnKernel_512_0
-  
-#pragma HLS INTERFACE m_axi port=merlin_input offset=slave depth=3326976 bundle=merlin_gmem_CnnKernel_128_0
-  
-#pragma HLS INTERFACE m_axi port=merlin_output offset=slave depth=200704 bundle=merlin_gmem_CnnKernel_512_0
-  
-#pragma HLS INTERFACE m_axi port=weight offset=slave depth=1638400 bundle=merlin_gmem_CnnKernel_32_0
-  
-#pragma HLS INTERFACE s_axilite port=bias bundle=control
-  
-#pragma HLS INTERFACE s_axilite port=merlin_input bundle=control
-  
-#pragma HLS INTERFACE s_axilite port=merlin_output bundle=control
-  
-#pragma HLS INTERFACE s_axilite port=weight bundle=control
-  
-#pragma HLS INTERFACE s_axilite port=return bundle=control
-  
-#pragma HLS DATA_PACK VARIABLE=merlin_output
-  
-#pragma HLS DATA_PACK VARIABLE=bias
-  
-#pragma HLS DATA_PACK VARIABLE=merlin_input
-  
-#pragma ACCEL interface variable=merlin_output depth=256,112,112 max_depth=256,112,112
-  
-#pragma ACCEL interface variable=bias depth=256 max_depth=256
-  
-#pragma ACCEL interface variable=weight depth=256,256,5,5 max_depth=256,256,5,5
-  
-#pragma ACCEL interface variable=merlin_input depth=256,228,228 max_depth=256,228,228
-  float bias_10_0_buf[256];
-  
-#pragma HLS array_partition variable=bias_10_0_buf cyclic factor=16 dim=1
-  float weight_16_0_buf[256][256][5][5];
-  
-#pragma HLS array_partition variable=weight_16_0_buf complete dim=4
-  
-#pragma HLS array_partition variable=weight_16_0_buf complete dim=3
-  float merlin_output_buf[256][112][112];
-  
-#pragma HLS array_partition variable=merlin_output_buf cyclic factor=16 dim=3
-// Allocate memory on heap to avoid stack overflow.
-  static float C[256][224][224];
-  
-#pragma HLS array_partition variable=C cyclic factor=2 dim=3
-  
-#pragma HLS array_partition variable=C cyclic factor=2 dim=2
-{
-    memcpy_wide_bus_read_float_512(&bias_10_0_buf[0],(class ap_uint< 512 > *)bias,(0 * 4),sizeof(float ) * ((unsigned long )256),256L);
-  }
-  merlinL15:
-  for (int i = 0; i < 256; ++i) {
-    merlinL14:
-    for (int h = 0; h < 224; ++h) {
-      merlinL13:
-      for (int w = 0; w < 224; ++w) 
-// Original: #pragma ACCEL PIPELINE AUTO
-{
-        
-#pragma HLS dependence variable=C array inter false
-        
-#pragma HLS pipeline
-        C[i][h][w] = bias_10_0_buf[i];
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kImSize; ++h) {
+      for (int w = 0; w < kImSize; ++w) {
+        C[i][h][w] = bias[i];
       }
     }
   }
-{
-    merlin_memcpy_0(weight_16_0_buf,0,0,0,0,weight,0,sizeof(float ) * ((unsigned long )1638400L),6553600UL);
-  }
-// Convolution
-  merlinL12:
-// Convolution
-  for (int i = 0; i < 256; ++i) {
-    merlinL11:
-    for (int j = 0; j < 256; ++j) {
-      float merlin_input_16_0_buf[228][228];
-      
-#pragma HLS array_partition variable=merlin_input_16_0_buf cyclic factor=5 dim=2
-      
-#pragma HLS array_partition variable=merlin_input_16_0_buf cyclic factor=5 dim=1
-{
-        memcpy_wide_bus_read_float_2d_228_128(merlin_input_16_0_buf,(::size_t )0,(::size_t )0,(class ap_uint< 128 > *)merlin_input,(::size_t )(((long )j) * 51984L * ((long )4)),sizeof(float ) * ((unsigned long )51984L),(::size_t )51984L);
-      }
-      merlinL10:
-      for (int h = 0; h < 224; ++h) {
-        merlinL9:
-        for (int w = 0; w < 224; ++w) 
-// Original: #pragma ACCEL PIPELINE AUTO
-{
-          
-#pragma HLS dependence variable=C array inter false
-          
-#pragma HLS pipeline
-          merlinL8:
-          for (int p = 0; p < 5; ++p) 
-// Original: #pragma ACCEL PARALLEL COMPLETE
-{
-            
-#pragma HLS unroll
-            merlinL7:
-            for (int q = 0; q < 5; ++q) 
-// Original: #pragma ACCEL PARALLEL COMPLETE
-{
-              
-#pragma HLS unroll
-              C[i][h][w] += weight_16_0_buf[i][j][p][q] * merlin_input_16_0_buf[h + p][q + w];
+
+  // Convolution
+  for (int i = 0; i < kNum; ++i) {
+    for (int j = 0; j < kNum; ++j) {
+      for (int h = 0; h < kImSize; ++h) {
+        for (int w = 0; w < kImSize; ++w) {
+          for (int p = 0; p < kKernel; ++p) {
+            for (int q = 0; q < kKernel; ++q) {
+              C[i][h][w] += weight[i][j][p][q] * input[j][h + p][w + q];
             }
           }
         }
       }
     }
-// Existing HLS partition: #pragma HLS array_partition variable=merlin_input_16_0_buf cyclic factor = 4 dim=2
   }
-// ReLU
-  merlinL6:
-// ReLU
-  for (int i = 0; i < 256; ++i) {
-    merlinL5:
-    for (int h = 0; h < 224; ++h) {
-      merlinL4:
-      for (int w = 0; w < 224; ++w) 
-// Original: #pragma ACCEL PIPELINE AUTO
-{
-        
-#pragma HLS dependence variable=C array inter false
-        
-#pragma HLS pipeline
-        if (((float )0) > C[i][h][w]) {
-          C[i][h][w] = ((float )0);
-        }
-         else {
-          C[i][h][w] = C[i][h][w];
+
+  // ReLU
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kImSize; ++h) {
+      for (int w = 0; w < kImSize; ++w) {
+        C[i][h][w] = max(0, C[i][h][w]);
+      }
+    }
+  }
+
+  // Max pooling
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kOutImSize; ++h) {
+      for (int w = 0; w < kOutImSize; ++w) {
+        output[i][h][w] = max(
+            max(C[i][h * 2][w * 2    ], C[i][h * 2 + 1][w * 2    ]),
+            max(C[i][h * 2][w * 2 + 1], C[i][h * 2 + 1][w * 2 + 1]));
+      }
+    }
+  }
+}
+
+extern "C" { 
+  // TODO: Modify the data types based on your kernel implementation.
+  void CnnKernel(class ap_uint< 128 > merlin_input[3326976],float weight[1638400],const class ap_uint< 512 > bias[16],class ap_uint< 512 > merlin_output[200704]);
+}
+
+int main(){
+  printf("Starting C-simulation...\n");
+  // input data
+  static float input[kNum][kInImSize][kInImSize];
+  // weight data
+  static float weight[kNum][kNum][kKernel][kKernel];
+  // bias data
+  static float bias[kNum];
+  // golden output data
+  static float outputSoftware[kNum][kOutImSize][kOutImSize];
+  // hardware output data
+  static float outputHardware[kNum][kOutImSize][kOutImSize];
+
+  // initialize input data
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kInImSize; ++h) {
+      for (int w = 0; w < kInImSize; ++w) {
+        input[i][h][w] = (float)rand() / RAND_MAX;
+      }
+    }
+  }
+
+  // initialize weight data
+  for (int i = 0; i < kNum; ++i) {
+    for (int j = 0; j < kNum; ++j) {
+      for (int p = 0; p < kKernel; ++p) {
+        for (int q = 0; q < kKernel; ++q) {
+          weight[i][j][p][q] = (float)rand() / RAND_MAX;
         }
       }
     }
   }
-// Max pooling
-  merlinL3:
-// Max pooling
-  for (int i = 0; i < 256; ++i) {
-    merlinL2:
-    for (int h = 0; h < 112; ++h) {
-      merlinL1:
-      for (int w = 0; w < 112; ++w) 
-// Original: #pragma ACCEL PIPELINE AUTO
-{
-        
-#pragma HLS dependence variable=merlin_output_buf array inter false
-        
-#pragma HLS pipeline
-        char rose__temp = (char )true;
-        float rose_temp;
-        if (C[i][h * 2][w * 2] > C[i][h * 2 + 1][w * 2]) {
-          rose_temp = C[i][h * 2][w * 2];
-        }
-         else {
-          rose_temp = C[i][h * 2 + 1][w * 2];
-        }
-        float rose_temp_0;
-        if (C[i][h * 2][w * 2 + 1] > C[i][h * 2 + 1][w * 2 + 1]) {
-          rose_temp_0 = C[i][h * 2][w * 2 + 1];
-        }
-         else {
-          rose_temp_0 = C[i][h * 2 + 1][w * 2 + 1];
-        }
-        rose__temp = ((char )((bool )(rose_temp > rose_temp_0)));
-        if ((bool )rose__temp) {
-          if (C[i][h * 2][w * 2] > C[i][h * 2 + 1][w * 2]) {
-            merlin_output_buf[i][h][w] = C[i][h * 2][w * 2];
-          }
-           else {
-            merlin_output_buf[i][h][w] = C[i][h * 2 + 1][w * 2];
-          }
-        }
-         else {
-          if (C[i][h * 2][w * 2 + 1] > C[i][h * 2 + 1][w * 2 + 1]) {
-            merlin_output_buf[i][h][w] = C[i][h * 2][w * 2 + 1];
-          }
-           else {
-            merlin_output_buf[i][h][w] = C[i][h * 2 + 1][w * 2 + 1];
-          }
+
+  // initialize bias data
+  for (int i = 0; i < kNum; ++i) {
+    bias[i] = (float)rand() / RAND_MAX;
+  }
+
+  // call the software golden model
+  CnnKernelSoftware(input, weight, bias, outputSoftware);
+  printf("Software finished!\n");
+
+  // call the hardware function
+  // TODO: Modify the data types based on your kernel implementation.
+  CnnKernel((ap_uint< 128 > *)input,(float *)weight,(ap_uint< 512 > *)bias,(ap_uint< 512 > *)outputHardware);
+  printf("Hardware finished!\n");
+
+  // compare the results of software and hardware
+  int err = 0;
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kOutImSize; ++h) {
+      for (int w = 0; w < kOutImSize; ++w) {
+        if(abs(outputSoftware[i][h][w] - outputHardware[i][h][w]) > 0.0001){
+          err++;
         }
       }
     }
   }
-// Existing HLS partition: #pragma HLS array_partition variable=merlin_output_buf cyclic factor = 16 dim=3
-  memcpy_wide_bus_write_float_3d_112_112_512((class ap_uint< 512 > *)merlin_output,merlin_output_buf,0,0,0,(4 * 0),sizeof(float ) * ((unsigned long )3211264L),3211264L);
+
+  if(err == 0){
+    printf("Test passed!\n");
+    return 0;
+  }else{
+    printf("Test failed with %d errors!\n", err);
+    return 1;
+  }
 }
-}
-// Existing HLS partition: #pragma HLS array_partition variable=bias_10_0_buf cyclic factor = 16 dim=1
